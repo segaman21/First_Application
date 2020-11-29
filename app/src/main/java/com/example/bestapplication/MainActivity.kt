927063a17046d6e03f3d4e7c3a80d1f056cc4add
segarman21 <sergey02047747@gmail.com>
2020-11-29 22:42:01 +0300

    Homework №3

--- app/src/main/java/com/example/bestapplication/MainActivity.kt
+++ app/src/main/java/com/example/bestapplication/MainActivity.kt
@@ -3,17 +3,26 @@ package com.example.bestapplication
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val start_second_activity = findViewById<Button>(R.id.start_second_activity)
-
-        start_second_activity.setOnClickListener{
-            val intent = Intent (this,MovieDetailsActivity::class.java)
-            startActivity(intent)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                    .add(R.id.fragment_container, FragmentMoviesList())
+                    .addToBackStack(null)
+                    .commit()
         }
     }
+    override fun onBackPressed() {
+        if (supportFragmentManager.backStackEntryCount > 0) {
+            supportFragmentManager.popBackStack()
+        } else {
+            super.onBackPressed()
+        }
+    }
+
 }
--- app/src/main/java/com/example/bestapplication/MovieDetailsActivity.kt
+++ app/src/main/java/com/example/bestapplication/MovieDetailsActivity.kt
@@ -7,5 +7,6 @@ class MovieDetailsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_movie_details)
+
     }
 }
